4119690c434c51bda61e1a532b142da011b89d74
Simon Jespersen <simjes91@me.com>
2020-05-26 10:53:36 +0200

    Implement Seek

--- lib/screens/Player.dart
+++ lib/screens/Player.dart
@@ -1,3 +1,5 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:mliss/components/custom-slider-thumb-shape.dart';
 import 'package:mliss/components/custom-slider-track-shape.dart';
@@ -10,7 +12,56 @@ class Player extends StatefulWidget {
 }
 
 class _PlayerState extends State<Player> {
+  static AudioCache audioCache = AudioCache(prefix: 'music/');
+  AudioPlayer player;
+  bool playing = false;
+  bool seeking = false;
+  double _sliderMin = 0;
+  double _sliderMax = 0;
   double _sliderValue = 0;
+
+  void play() async {
+    player = await audioCache.play(
+        'Michael Oakley - California - 03 California (feat. Missing Words).mp3');
+
+    player.onAudioPositionChanged.listen((event) {
+      if (!seeking) {
+        setState(() {
+          _sliderValue = event.inMilliseconds.toDouble();
+        });
+      }
+    });
+
+    final duration = await player.getDuration();
+
+    setState(() {
+      playing = true;
+      _sliderMax = duration.toDouble();
+    });
+  }
+
+  void seek(int position) async {
+    final duration = Duration(milliseconds: position);
+    await player.seek(duration);
+
+    setState(() {
+      seeking = false;
+    });
+  }
+
+  void resume() {
+    player.resume();
+    setState(() {
+      playing = true;
+    });
+  }
+
+  void pause() {
+    player.pause();
+    setState(() {
+      playing = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +69,8 @@ class _PlayerState extends State<Player> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          SizedBox(
-            // TODO: temp placeholder for record
-            height: 400,
+          Container(
+            child: Image(image: AssetImage('assets/music/cover.jpg')),
           ),
           Padding(
             padding: EdgeInsets.only(left: 20, right: 20),
@@ -49,8 +99,17 @@ class _PlayerState extends State<Player> {
                   ),
                   child: Slider(
                     value: _sliderValue,
-                    min: 0,
-                    max: 100,
+                    min: _sliderMin,
+                    max: _sliderMax,
+                    onChangeStart: (position) {
+                      setState(() {
+                        seeking = true;
+                      });
+                    },
+                    onChangeEnd: (position) {
+                      final playTime = position.toInt();
+                      seek(playTime);
+                    },
                     onChanged: (double value) {
                       setState(() {
                         _sliderValue = value;
@@ -59,7 +118,13 @@ class _PlayerState extends State<Player> {
                   ),
                 ),
                 Row(
-                  children: <Widget>[],
+                  children: <Widget>[
+                    playing
+                        ? IconButton(icon: Icon(Icons.pause), onPressed: pause)
+                        : IconButton(
+                            icon: Icon(Icons.play_arrow),
+                            onPressed: player == null ? play : resume)
+                  ],
                 )
               ],
             ),
