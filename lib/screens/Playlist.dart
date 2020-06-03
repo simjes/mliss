8d725d7a39536cd0a6546c7559e8d632d07f29d3
Simon Jespersen <simjes91@me.com>
2020-06-03 12:49:24 +0200

    Add Futurebuilder to playlists

--- lib/screens/Playlist.dart
+++ lib/screens/Playlist.dart
@@ -1,38 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_simple_dependency_injection/injector.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:mliss/constants.dart';
 import 'package:mliss/models/PlaylistDto.dart';
 import 'package:mliss/services/spotify.dart';
 
 class Playlists extends HookWidget {
   static const route = '/playlists';
+  final spotifyService = Injector.getInjector().get<SpotifyService>();
 
   @override
   Widget build(BuildContext context) {
-    final playlists = useState<List<PlaylistDto>>([]);
-
-    loadPlaylists({int offset = 0}) async {
-      var spotifyService = Injector.getInjector().get<SpotifyService>();
-      var result = await spotifyService.getMyPlaylists(offset: offset);
-
-      playlists.value.addAll(result.items);
-    }
-
-    useEffect(() {
-      loadPlaylists();
-      return null;
-    }, []);
-
     return Scaffold(
-      body: ListView.builder(
-        itemCount: playlists.value.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(
-              playlists.value[index].name,
-              style: TextStyle(color: Colors.white),
-            ),
-          );
+      body: FutureBuilder(
+        future: spotifyService.getMyPlaylists(limit: 50),
+        builder: (BuildContext context, AsyncSnapshot<PlaylistsDto> snapshot) {
+          if (snapshot.hasData) {
+            final playlists = snapshot.data.items;
+
+            return ListView.builder(
+              itemCount: snapshot.data.items.length,
+              itemBuilder: (context, index) {
+                final playlist = playlists[index];
+
+                return Card(
+                  child: ListTile(
+                    leading: Image(
+                      image: NetworkImage(playlist.images[0].url),
+                    ),
+                    title: Text(
+                      playlist.name,
+                    ),
+                    subtitle: Text(
+                        'by ${playlist.owner.displayName}, ${playlist.tracks.total} tracks'),
+                  ),
+                );
+              },
+            );
+          }
+
+          if (snapshot.hasError) {
+            return Text('placeholder error');
+          }
+
+          return SpinKitWave(color: kSliderHandleColor);
         },
       ),
     );
